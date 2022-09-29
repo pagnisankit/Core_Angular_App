e9d1eb018af8f8e6e804d50756fdd26a09f30368
Pagnis <ankit_pagnis@yahoo.com>
2022-09-30 00:32:44 +0530

    added auth guard, member-detail, member-list, list

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../_models/model';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +14,8 @@ export class NavComponent implements OnInit {
   model : any = {}
  currentUser$: Observable<User>;
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private router: Router
+      , private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.currentUser$ = this.accountService.currentUser$;
@@ -21,13 +24,16 @@ export class NavComponent implements OnInit {
   login(){
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
+      this.router.navigateByUrl('/members');
     }, error => {
-      console.log(error)
+      console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
   
